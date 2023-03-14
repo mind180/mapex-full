0504e983b83c760fe49d98884f84307b7d1a5dcf
billjoynf <45283458+billjoynf@users.noreply.github.com>
2023-03-14 18:15:56 +0200

    impl: add base logic

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,9 +1,19 @@
+const User = require('../models/User')
+const Role = require('../models/Role')
+
 class authController {
     async registration(req, res) {
         try { 
+            const { email, password } = req.body
+            const existingUser = await User.findOne({ email })
+            if (existingUser) {
+                res.status(400).json({ message: 'User already exist' })
+            }
 
+            const user = new User({ email, password })
         } catch(e) {
-
+            console.error(e)
+            res.status(400).json({ message: 'Internal server error'})
         }
     }
 
@@ -11,15 +21,17 @@ class authController {
         try { 
 
         } catch(e) {
-            
+            console.error(e)
+            res.status(400).json({ message: 'Internal server error'})
         }
     }
 
     async getUsers(req, res) {
-        try { 
-
+        try {
+            const adminRole = await Role.findOne({ value: 'Admin' })
+            res.send(adminRole)
         } catch(e) {
-            
+            console.error(e);
         }
     }
 }
