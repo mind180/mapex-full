66542af5c5c30b1a1735485b56fed279faed9e52
billjoynf <45283458+billjoynf@users.noreply.github.com>
2023-03-14 18:16:54 +0200

    new: auth router

--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -1,9 +1,9 @@
-const Router = require('express')
-const router = new Router()
+const { Router } = require('express')
 const controller = require('../controllers/authController')
 
+const router = new Router()
 router.post('/registration', controller.registration)
 router.post('/login', controller.login)
-router.post('./users', controller.getUsers)
+router.get('/users', controller.getUsers)
 
 module.exports = router
