a7f7cc9024a94011f8c9acfd2bb764bec88dc8e1
billjoynf <45283458+billjoynf@users.noreply.github.com>
2023-03-14 18:20:51 +0200

    refactor: inddex

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const authRouter = require('./routers/authRouter')
+
 const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use(express.json())
+app.use('/auth', authRouter)
 
 function start() {
     try {
