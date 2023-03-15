d465dd1686c4f94503dacd7b746cd21b60e70c9e
billjoynf <45283458+billjoynf@users.noreply.github.com>
2023-03-15 21:57:19 +0200

    implement auth middleware

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,16 +1,39 @@
 const User = require('../models/User')
 const Role = require('../models/Role')
+const bcrypt = require('bcryptjs')
+const { validationResult } = require('express-validator')
+const jwt = require('jsonwebtoken')
+const { secret } = require('../configs/config')
+
+function generateAccressToken(id, roles) {
+    const payload = {
+        id,
+        roles 
+    }
+    return jwt.sign(payload, secret, { expiresIn: '24h' })
+}
 
 class authController {
     async registration(req, res) {
-        try { 
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ message: 'Validation error', error: errors })
+            }
+
             const { email, password } = req.body
             const existingUser = await User.findOne({ email })
             if (existingUser) {
-                res.status(400).json({ message: 'User already exist' })
+                return res.status(400).json({ message: 'User already exist' })
             }
 
-            const user = new User({ email, password })
+            const userRole = await Role.findOne({ value: 'Admin' })
+            
+            const hashPassword = bcrypt.hashSync(password, 9)
+            const user = new User({ email, password: hashPassword, roles: [ userRole.value ] })
+            await user.save()
+
+            res.json({ message: 'user has been created' })
         } catch(e) {
             console.error(e)
             res.status(400).json({ message: 'Internal server error'})
@@ -18,8 +41,20 @@ class authController {
     }
 
     async login(req, res) {
-        try { 
+        try {
+            const { email, password } = req.body
+            const user = await User.findOne({ email })
+            if (!user) {
+                return res.status(404).json({ message: 'Wrong email or password' })
+            }
 
+            const isPasswordValid = bcrypt.compareSync(password, user.password)
+            if (!isPasswordValid) {
+                return res.status(403).json({ message: 'Wrong email or password' })
+            }
+
+            const token = generateAccressToken(user._id, user.roles)
+            res.json(token)
         } catch(e) {
             console.error(e)
             res.status(400).json({ message: 'Internal server error'})
@@ -28,8 +63,8 @@ class authController {
 
     async getUsers(req, res) {
         try {
-            const adminRole = await Role.findOne({ value: 'Admin' })
-            res.send(adminRole)
+            const users = await User.find({})
+            res.send(users)
         } catch(e) {
             console.error(e);
         }
