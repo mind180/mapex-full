8ffd1143f013fc30f89befbde34dd1741fb28c9e
billjoynf <45283458+billjoynf@users.noreply.github.com>
2023-03-19 00:00:05 +0200

    init: board controller

---
+++ backend/controllers/boardController.js
@@ -0,0 +1,33 @@
+const Board = require('../models/Board')
+
+function dublicateId(documents) {
+    
+}
+
+class BoardController {
+    async getBoards(req, res) {
+        try {
+            const boards = await Board.find({})
+            console.log(boards);
+            dublicateId(boards)
+            res.send(boards)
+        } catch(e) {
+            console.error(e);
+            res.status(400).json({ message: 'Internal server error' })
+        }
+    }
+
+    async getBoard(req, res) {
+        try {
+            const boardId = req.params.boardId
+            console.log(boardId);
+            const board = await Board.findOne({ _id: boardId })
+            res.send(board)
+        } catch(e) {
+            console.error(e);
+            res.status(400).json({ message: 'Internal server error' })
+        }
+    }
+}
+
+module.exports = new BoardController()
