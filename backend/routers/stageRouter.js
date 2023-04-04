5acea15256d70e157f984701a3a82b2552add285
billjoynf <45283458+billjoynf@users.noreply.github.com>
2023-04-04 16:37:32 +0300

    fix: api param name

--- backend/routers/stageRouter.js
+++ backend/routers/stageRouter.js
@@ -2,9 +2,9 @@ const { Router } = require('express')
 const stageController = require('../controllers/stageController')
 
 const router = new Router({})
-router.get('/maps/:mapsId/stages', stageController.getStages)
-router.post('/maps/:mapsId/stages', stageController.saveStages)
-router.put('/maps/:mapsId/stages/:stageId', stageController.updateStage)
-router.delete('/maps/:mapsId/stages/:stageId', stageController.deleteStage)
+router.get('/maps/:mapId/stages', stageController.getStages)
+router.post('/maps/:mapId/stages', stageController.saveStages)
+router.put('/maps/:mapId/stages/:stageId', stageController.updateStage)
+router.delete('/maps/:mapId/stages/:stageId', stageController.deleteStage)
 
 module.exports = router
