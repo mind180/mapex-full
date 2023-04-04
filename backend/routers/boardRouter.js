b82e06d1a3337ec3a5bb91e51074ce14a6d41820
billjoynf <45283458+billjoynf@users.noreply.github.com>
2023-04-04 11:04:44 +0300

    refactor: rename board to map

--- backend/routers/boardRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express')
-const boardController = require('../controllers/boardController')
-
-const router = new Router({})
-router.get('/canvas', boardController.getBoards)
-router.get('/canvas/:boardId', boardController.getBoard)
-
-module.exports = router
