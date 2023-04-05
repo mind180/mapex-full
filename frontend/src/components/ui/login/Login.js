858ba542c3fd91eca06382139fe40cb1c1611aee
billjoynf <45283458+billjoynf@users.noreply.github.com>
2023-04-05 21:34:45 +0300

    implement stub for right column of login page

--- frontend/src/components/ui/login/Login.js
+++ frontend/src/components/ui/login/Login.js
@@ -3,6 +3,7 @@ import LoginLeft from "./LoginLeft";
 import LoginRight from "./LoginRight";
 import './Login.css';
 
+//center left and right content
 export default function Login(props) {
     return <div className="login">
         <LoginLeft />
--- frontend/src/components/ui/login/LoginRight.js
+++ frontend/src/components/ui/login/LoginRight.js
@@ -1,6 +1,27 @@
 import React from "react";
-import "./Login.css"
+import "./Login.css";
+import plan from './Plan.png';
 
+//fix this stupid class names
 export default function LoginRight(props) {
-    return <div className="login-adv"></div>
+    return <div className="login-right">
+        <div className="login-right-content">
+            <div className="right-content-main">
+                <div className="promo-img-block">
+                    <img className="promo-img" src={plan} />
+                </div>
+            </div>
+            <div className="right-content-footer">
+                <h3 className="description-title">Build a plan</h3>
+                <div className="description-text">
+                    Build your path or take someone else's experience to achieve your goals
+                </div>
+                <div className="description-scroll">
+                    <div className="prev"></div>
+                    <div className="scroll-index">⦿ ○ ○</div>
+                    <div className="next"></div>
+                </div>
+            </div>
+        </div>
+    </div>
 }
