abdf38d060a5ea07a469fa5992f097e7a88a1585
billjoynf <45283458+billjoynf@users.noreply.github.com>
2023-04-06 23:31:15 +0300

    fix: auth router pathes

--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -1,5 +1,5 @@
 const { Router } = require('express')
-const controller = require('../controllers/authController')
+const authController = require('../controllers/authController')
 const { check } = require('express-validator')
 const authMiddleware = require('../middlewares/authMiddleware')
 const roleMiddleware = require('../middlewares/roleMiddleware')
@@ -8,8 +8,8 @@ const router = new Router()
 router.post('/registration', [
     check('email', 'Invalid email format').isEmail(),
     check('password', 'Length must be from 8 to 16').isLength({ min: 8, max: 16 })
-], controller.registration)
-router.post('/login', controller.login)
-router.get('/users', roleMiddleware([ 'Admin' ]), controller.getUsers)
+], authController.registration)
+router.get('/users', roleMiddleware([ 'Admin' ]), authController.getUsers)
+router.post('/login', authController.login)
 
 module.exports = router
