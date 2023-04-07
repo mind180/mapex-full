e878c63d44388db8b949f839764ad6c23f00b6e7
billjoynf <45283458+billjoynf@users.noreply.github.com>
2023-04-07 20:05:25 +0300

    add protection for private routs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/ui/header/Header.js';
 import CanvasStateProvider from './components/wrappers/CanvasStateProvider.js';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 import ProfileHeader from './components/ui/profile-header/ProfileHeader.js';
 import Dashboard from "./components/ui/dashboard/Dashboard";
 import Board from "./components/ui/board/Board";
 import Login from "./components/ui/login/Login";
+import { getCookie, AUTH_TOKEN_NAME } from './api/cookie';
+import ProtectedRout from './components/wrappers/ProtectedRoute';
+
 
 function App() {
   return (
@@ -17,21 +20,29 @@ function App() {
             <Login />
           </Route>
           <Route path="/library">
-            <Header />
-            <Dashboard />
+              <ProtectedRout isAuthenticated={getCookie(AUTH_TOKEN_NAME)}>
+                <Header />
+                <Dashboard />
+              </ProtectedRout>
           </Route>
           <Route path="/canvas/:canvasId">
-            <Header />
-            <Board/>
+            <ProtectedRout isAuthenticated={getCookie(AUTH_TOKEN_NAME)}>
+              <Header />
+              <Board/>
+            </ProtectedRout>
           </Route>
           <Route path="/user/:userId">
-            <Header />
-            <ProfileHeader />
-            <Dashboard />
+            <ProtectedRout isAuthenticated={getCookie(AUTH_TOKEN_NAME)}>
+              <Header />
+              <ProfileHeader />
+              <Dashboard />
+            </ProtectedRout>
           </Route>
           <Route path="/">
-            <Header />
-            <CanvasStateProvider canvasId='641367d3e6e00f461f2135ea'/>
+            <ProtectedRout isAuthenticated={getCookie(AUTH_TOKEN_NAME)}>
+              <Header />
+              <CanvasStateProvider canvasId='641367d3e6e00f461f2135ea'/>
+            </ProtectedRout>
           </Route>
         </Switch>
       </Router>
