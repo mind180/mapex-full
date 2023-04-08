01526a2b3b1199b35fca5c0f03fbb7fef1a5c74c
billjoynf <45283458+billjoynf@users.noreply.github.com>
2023-04-08 03:18:58 +0300

    implement register component

---
+++ frontend/src/components/ui/login/Registration.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RegistrationForm from "./RegistrationForm";
+import LoginRight from "./LoginRight";
+
+export default function Registration() {
+    return (
+        <div className="login">
+            <RegistrationForm />
+            <LoginRight />
+        </div>
+    )
+}
