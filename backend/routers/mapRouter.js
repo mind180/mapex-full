eec003cbb8ae0b0f5644029c6e3192d047a680d0
billjoynf <45283458+billjoynf@users.noreply.github.com>
2023-04-08 16:48:38 +0300

    add auth middleware before map routs

--- backend/routers/mapRouter.js
+++ backend/routers/mapRouter.js
@@ -1,7 +1,9 @@
 const { Router } = require('express')
 const mapController = require('../controllers/mapController')
+const authMiddleware = require('../middlewares/authMiddleware')
 
 const router = new Router({})
+router.use(authMiddleware)
 router.get('/maps', mapController.getMaps)
 router.get('/maps/:mapId', mapController.getMap)
 router.post('/maps', mapController.createMap)
