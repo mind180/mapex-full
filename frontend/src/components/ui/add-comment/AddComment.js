4596ec8a52af5ba69603db25cdc6fc03cb264d64
billjoynf <45283458+billjoynf@users.noreply.github.com>
2023-04-26 01:07:09 +0300

    implement adding new comments

--- frontend/src/components/ui/add-comment/AddComment.js
+++ frontend/src/components/ui/add-comment/AddComment.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import './AddComment.css';
+import { processEntity } from "../../../api/api";
 
 export default function AddComment(props) {
+	const [commentText, setCommentText] = useState('');
 
+	const saveComment = () => {
+		const comment = {
+			text: commentText
+		};
+
+		processEntity('POST', `/maps/${props.mapId}/comment`, comment)
+			.then(res => res.json())
+			.then(newComment => props.onCommentAdd(newComment))
+			.catch(e => console.error(e));
+	}
 
 	return (
 		<div className="add-comment">
 			<main className="add-comment-body">
 				<div className="add-comment-avatar"></div>
-				<textarea className="add-comment-field" ></textarea>
+				<textarea className="add-comment-field" onChange={e => setCommentText(e.target.value)}></textarea>
 			</main>
 			<footer className="add-comment-footer">
-				<input className="add-comment-button" type="button" value='Add comment' />
+				<input className="add-comment-button" type="button" onClick={saveComment} value='Add comment' />
 			</footer>
 		</div>
 	)
