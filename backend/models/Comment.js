6042b09bafe788e21f4c02ac59647407a6eedebd
billjoynf <45283458+billjoynf@users.noreply.github.com>
2023-04-26 01:13:59 +0300

    add field to comment model

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -3,6 +3,10 @@ const { Schema, model } = require('mongoose')
 const commentSchema = new Schema({
     text: String,
     date: Date,
+    board: {
+        type: Schema.Types.ObjectId,
+        ref: 'Board'
+    },
     user: {
         type: Schema.Types.ObjectId,
         ref: 'User'
