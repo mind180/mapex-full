a4b9b936097d43e566ce9cb9f10cb1065332bdf4
billjoynf <45283458+billjoynf@users.noreply.github.com>
2023-05-03 23:29:16 +0300

    fix mix height

--- frontend/src/canvas/services/resize-service/lib/common.js
+++ frontend/src/canvas/services/resize-service/lib/common.js
@@ -1,6 +1,6 @@
 export const zoneSize = 300;
 export const minWidth = zoneSize * 3;
-export const minHeight = zoneSize * 3;
+export const minHeight = zoneSize;
 
 export function getMostDownNode(nodes) {
   return nodes.sort((a, b) => b.position.y - a.position.y)[0];
