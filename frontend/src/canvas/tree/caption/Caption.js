3eb1b0d28347bc969a7853c10974235bae4eb529
billjoynf <45283458+billjoynf@users.noreply.github.com>
2023-05-03 23:18:52 +0300

    add new map captions with specific actions

--- frontend/src/canvas/tree/caption/Caption.js
+++ frontend/src/canvas/tree/caption/Caption.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import './Caption.css';
 
 export default function Caption(props) {
+  const history = useHistory();
+
+  const back = () => {
+		history.push(`/map/${props.mapId}/passing`);
+	}
+
+	const save = () => {
+		history.push(`/map/${props.mapId}/passing`);
+	}
+
   return (
-      <header className='canvas-header'>
-        <div className='header-title'>{props.title || 'Title'}</div>
-        <div className='header-description'>{props.description || 'Some description'}</div>
-      </header>
+      <div className='canvas-header-container'>
+        <header className='canvas-header'>
+          <div className='header-about'>
+            <div className='header-title'>{props.title || 'Title'}</div>
+            <div className='header-description'>{props.description || 'Some description'}</div>
+          </div>
+          <div className='header-actions'>
+            <input className='header-nav-button' type='button' value='← Back' onClick={back} />
+            <input className='header-nav-button' type='button' value='🖫 Save' onClick={save}/>
+          </div>
+        </header>
+      </div>
   );
 }
