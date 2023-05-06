9b896b49b982eca1de346af3944b72ab14067e8b
billjoynf <45283458+billjoynf@users.noreply.github.com>
2023-05-06 03:26:54 +0300

    add methods for file uploads

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const UserAvatar = require('../models/UserAvatar')
 
 class UserController {
     async getYourUser(req, res) {
@@ -18,6 +19,33 @@ class UserController {
             res.status(200).json({ message: 'Updated succesfully' })
         } catch(e) {
             console.error(e);
+        }
+    }
+
+    async uploadAvatar(req, res) {
+        try {
+            const userId = req.user.id
+            const userAvatarBuffer = req.body
+            const avatar = { $set: { value: userAvatarBuffer } }
+            await UserAvatar.updateOne({ user: userId }, avatar, { upsert: true })
+            res.status(200).json({ message: 'Uploaded succesfully' })
+        } catch(e) {
+            console.error(e);
+        }
+    }
+
+    async getUserAvatar(req, res) {
+        try {
+            const userId = req.user.id
+            const userAvatar = await UserAvatar.findOne({ user: userId }).select('value')
+            if (!userAvatar) {
+                return res.status(404).json({ message: 'Image is not found' })
+            }
+            res.set('Content-Type', 'image/png');
+            res.send(userAvatar.value)
+        } catch(e) {
+            console.error(e);
+            res.status(400).json({ message: 'Internal server error' })
         }
     }
 }
--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -5,7 +5,8 @@ const authMiddleware = require('../middlewares/authMiddleware')
 const router = new Router({})
 router.use(authMiddleware)
 router.get('/user/you', userController.getYourUser)
+router.get('/user/you/avatar', userController.getUserAvatar)
 router.put('/user/you', userController.updateUsername)
-
+router.post('/user/you/avatar', userController.uploadAvatar)
 
 module.exports = router
