712ad7ce09466883cad7717d41290e99d1ac1480
billjoynf <45283458+billjoynf@users.noreply.github.com>
2023-05-06 05:31:08 +0300

    add user avatar in add comment component

--- frontend/src/components/ui/add-comment/AddComment.js
+++ frontend/src/components/ui/add-comment/AddComment.js
@@ -1,26 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import './AddComment.css';
 import { processEntity } from "../../../api/api";
 
 export default function AddComment(props) {
+	const avatarRef = useRef();
+	const inputRef = useRef();
 	const [commentText, setCommentText] = useState('');
 
+	useEffect(() => {
+		processEntity('GET', `/user/you/avatar`)
+		  .then(response => response.json())
+		  .then(avatar => avatarRef.current.src = avatar)
+		  .catch(error => console.error(error))
+	}, []);
+
 	const saveComment = () => {
-		const comment = {
-			text: commentText
-		};
+		const comment = { text: commentText };
 
 		processEntity('POST', `/maps/${props.mapId}/comment`, comment)
 			.then(res => res.json())
 			.then(newComment => props.onCommentAdd(newComment))
+			.then(() => inputRef.current.value = '')
 			.catch(e => console.error(e));
 	}
 
 	return (
 		<div className="add-comment">
 			<main className="add-comment-body">
-				<div className="add-comment-avatar"></div>
-				<textarea className="add-comment-field" onChange={e => setCommentText(e.target.value)}></textarea>
+				<div className="add-comment-avatar">
+					<img className='add-comment-avatar-img' ref={avatarRef} />
+				</div>
+				<textarea ref={inputRef} className="add-comment-field" onChange={e => setCommentText(e.target.value)}></textarea>
 			</main>
 			<footer className="add-comment-footer">
 				<input className="add-comment-button" type="button" onClick={saveComment} value='Add comment' />
