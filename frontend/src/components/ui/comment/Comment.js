326bb006b6bb5b6fac6a35023cca32fcf13c5ce5
billjoynf <45283458+billjoynf@users.noreply.github.com>
2023-05-06 06:19:49 +0300

    added: avatar image

--- frontend/src/components/ui/comment/Comment.js
+++ frontend/src/components/ui/comment/Comment.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import './Comment.css';
+import { processEntity } from "../../../api/api";
 
 export default function Comment(props) {
+	const avatarRef = useRef();
+
+	useEffect(() => {
+		processEntity('GET', `/user/${props.commentData.user._id}/avatar`)
+		  .then(response => response.json())
+		  .then(avatar => avatarRef.current.src = avatar)
+		  .catch(error => console.error(error))
+	}, []);
+
 	const handleLike = () => {
 		alert('In progress ...');
 	}
@@ -13,11 +23,13 @@ export default function Comment(props) {
 
 	return (
 		<div className="comment">
-			<div className="comment-avatar"></div>
+			<div className="comment-avatar">
+				<img className='comment-avatar-img' ref={avatarRef} />
+			</div>
 			<main className="comment-main">
 				<div className="comment-main-header">
 					<h4 className="comment-username">
-						Username
+						{props.commentData.user.username}
 					</h4>
 					<div className="comment-date">
 						{ props.commentData.date }
