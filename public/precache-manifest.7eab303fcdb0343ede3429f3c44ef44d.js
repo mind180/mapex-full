883bd4c34914df01cb3c43dfd5547aa4cccd7019
billjoynf <45283458+billjoynf@users.noreply.github.com>
2023-05-12 06:10:43 +0300

    move files from backend/public to root

---
+++ public/precache-manifest.7eab303fcdb0343ede3429f3c44ef44d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232c55e55deb6e861b9b973ecf1e6c50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9463d16d2a4ad9f8baad",
+    "url": "/static/css/main.5e1ad8e3.chunk.css"
+  },
+  {
+    "revision": "229966d0121986d17ad2",
+    "url": "/static/js/2.0ded5089.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.0ded5089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9463d16d2a4ad9f8baad",
+    "url": "/static/js/main.dd171ca3.chunk.js"
+  },
+  {
+    "revision": "7ec35e66ed7c98364a88",
+    "url": "/static/js/runtime-main.2c8413c0.js"
+  },
+  {
+    "revision": "1eb796602f170110e4abfd52cc2f08b0",
+    "url": "/static/media/Logo6.1eb79660.jpg"
+  },
+  {
+    "revision": "2b80226e83ae60edc7de92036b14731e",
+    "url": "/static/media/Plan2.2b80226e.png"
+  }
+]);
